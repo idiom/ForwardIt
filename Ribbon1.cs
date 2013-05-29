e7203c2299d1332960dfdd9a978046bde26ca107
Sean Wilson <sean@idiom.ca>
2013-05-28 21:30:05 -0400

    Initial Commit

---
+++ Ribbon1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Office = Microsoft.Office.Core;
+
+
+namespace ForwardIt
+{
+    [ComVisible(true)]
+    public class Ribbon1 : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+        private string email = string.Empty;
+
+
+        public Ribbon1(string email)
+        {
+            this.email = email;
+        }
+
+
+        private void ProcessMail()
+        {
+            try
+            {
+                var olapp = new Microsoft.Office.Interop.Outlook.Application();
+                Object selObject = olapp.ActiveExplorer().Selection[1];
+
+                if (selObject is Microsoft.Office.Interop.Outlook.MailItem)
+                {
+                    Microsoft.Office.Interop.Outlook.MailItem mailItem = (selObject as Microsoft.Office.Interop.Outlook.MailItem);
+                    var fwdmail = mailItem.Forward();
+                    fwdmail.Recipients.Add(this.email);
+                    fwdmail.Send();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                //todo log this.
+
+            }
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("ForwardIt.RibbonUIDef.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks        
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        public void OnClick(Office.IRibbonControl control)
+        {
+            //Process the mail.
+            ProcessMail();
+        }
+        
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ ThisAddIn.cs
@@ -0,0 +1,69 @@
+﻿
+using Microsoft.Win32;
+using System;
+namespace ForwardIt
+{
+    public partial class ThisAddIn
+    {
+
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new Ribbon1(GetEmailFromReg());
+        }
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            this.PluginInit();
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+
+
+        }
+
+        private void PluginInit()
+        {
+            //init code here
+        }
+
+
+        private string GetEmailFromReg()
+        {                                    
+            RegistryKey sk1 = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\OutlookSample");            
+            string keyname = "email";
+            
+            if ( sk1 == null )
+            {
+                //Couldn't load the reg key
+                throw new Exception("Email Not configured!");
+            }
+            else
+            {
+                try 
+                {                    
+                    return (string)sk1.GetValue(keyname);
+                }
+                catch (Exception ex)
+                {                                        
+                    return null;
+                }
+            }
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
